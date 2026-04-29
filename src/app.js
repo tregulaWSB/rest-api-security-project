a98f0cdbec95932787fe4c6565f2eb7b51dab4a2
tregula124 <tomeeek124@onet.pl>
2026-04-29 17:15:19 +0200

    Added HTTPS possibility; created app script for easy server start

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
+// const fs = require('fs'); // for HTTPS
+// const https = require('https'); // for HTTPS
 
 // db connection
 const { connectDB } = require('./config/db');
@@ -16,10 +18,11 @@ const { morganMiddleware } = require('./middleware/morganMiddleware');
 const { errorHandler } = require('./middleware/errorHandler');
 const { globalRateLimiter } = require('./middleware/rateLimiter');
 
-// utils/models
+// utils or models
 const { logger } = require('./utils/logger');
-const { deleteExpiredTokens } = require('./model/refreshTokens')
+const { deleteExpiredTokens } = require('./model/refreshTokens');
 
+// helmet configuration for security and proper Swagger access
 app.use(helmet({
   contentSecurityPolicy: {
     directives: {
@@ -36,39 +39,64 @@ app.use(helmet({
 }));
 app.disable('x-powered-by');
 
+// CORS configuration
 app.use(cors({
   origin: process.env.ALLOWED_ORIGIN || 'http://localhost:3000',
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+// morgan middleware for event logging
 app.use(morganMiddleware);
 
+// global rate limiter
 app.use((globalRateLimiter));
 
+// json payload upper limit
 app.use(express.json({ limit: '10kb' }));
 
 const API_PREFIX = '/api/v1';
 
+// Swagger route
 app.use('/docs', docsRouter);
+// app routes
 app.use(`${API_PREFIX}/auth`, authRouter);
 app.use(`${API_PREFIX}/posts`, postRouter);
-
+// any other routes
 app.use((req, res) => {
   res.status(404).json({ msg: 'not found' });
 });
 
+// error handler at the end
 app.use(errorHandler);
 
-const port = process.env.PORT || 3000;;
+// HTTP server
+const port = process.env.PORT || 3000;
 const startServer = async () => {
   await connectDB();
   logger.info('Successfully established connection to database');
   await deleteExpiredTokens();
-  logger.info('Deleted all expired refresh_tokens from database')
+  logger.info('Deleted all expired refresh_tokens from database');
   app.listen(port, () => 
     logger.info('Server started', { port })
   );
 }
+
+// for HTTPS
+// const options = {
+//     pfx: fs.readFileSync(process.env.CERTIFICATE_PATH),
+//     passphrase: process.env.CERTIFICATE_PASSWORD
+// };
+
+// const port = process.env.PORT || 443;
+// const startServer = async () => {
+//   await connectDB();
+//   logger.info('Successfully established connection to database');
+//   await deleteExpiredTokens();
+//   logger.info('Deleted all expired refresh_tokens from database');
+//   https.createServer(options, app).listen(443, () => 
+//     logger.info('Server started with HTTPS', { port: 443 })
+//   );
+// }
 
 startServer();
